fe526d1f912137168b66a31364c783ed27ccf889
dhanashwar <dhanashwarkumar25243@gmail.com>
2024-04-03 11:22:57 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+
+export const geoApiOptions  = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '',
+		
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "";
